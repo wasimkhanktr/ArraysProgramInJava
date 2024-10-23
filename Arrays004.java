1758eaf219110bd8ae13e0ce6b60440479f0d755
MD WASIM AKRAM <wasimkhanktr@gmail.com>
2024-10-24 01:57:58 +0530

    Finding the Length of an Array

---
+++ Arrays004.java
@@ -0,0 +1,13 @@
+package arrays.java.zn;
+
+public class Arrays004 {
+
+	public static void main(String[] args) {
+		int a[] = {1,2,3,4,5,6,7};
+		int length=a.length;
+		
+		System.out.println("length of arrays is "+length);
+
+	}
+
+}
