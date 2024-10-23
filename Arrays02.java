7f73c7b9151660248f851493371aaefae79efa0c
MD WASIM AKRAM <wasimkhanktr@gmail.com>
2024-10-24 01:45:19 +0530

    Accessing Elements of an Array

---
+++ Arrays02.java
@@ -0,0 +1,12 @@
+package arrays.java.zn;
+
+public class Arrays02 {
+
+	public static void main(String[] args) {
+		int arr[]= {1,2,3,4};
+		
+		System.out.println("fourth elements "+arr[3]);
+		System.out.println("first elements "+arr[0]);
+	}
+
+}
