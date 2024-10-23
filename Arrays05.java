8d38694dff0651cf8035e9dfa97cc40c4f0c6df0
MD WASIM AKRAM <wasimkhanktr@gmail.com>
2024-10-24 02:12:04 +0530

    Looping through an Array

---
+++ Arrays05.java
@@ -0,0 +1,16 @@
+package arrays.java.zn;
+
+public class Arrays05 {
+
+	public static void main(String[] args) {
+		
+		int a[] = {1,2,3,4,5,6};
+		
+		for(int i=0; i<a.length; i++)
+		{
+			System.out.println("Element at Index "+i+" : "+a[i]);
+		}
+
+	}
+
+}
---
+++ Arrays06.java
@@ -0,0 +1,10 @@
+package arrays.java.zn;
+
+public class Arrays06 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
