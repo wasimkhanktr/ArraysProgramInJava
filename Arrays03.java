8775c2ccfe042d0c401f7feb98d3279c81d7e510
MD WASIM AKRAM <wasimkhanktr@gmail.com>
2024-10-24 01:52:08 +0530

    Modifying Elements of an Array

---
+++ Arrays03.java
@@ -0,0 +1,15 @@
+package arrays.java.zn;
+
+import java.util.Arrays;
+
+public class Arrays03 {
+
+	public static void main(String[] args) {
+		int arr[]= {1,2,3,4};
+		System.out.println("old arrays : "+Arrays.toString(arr));
+		arr[1]=9;
+		System.out.println("new arrays : "+Arrays.toString(arr));
+
+	}
+
+}
