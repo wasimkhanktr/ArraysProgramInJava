14394512baedaefc5e48766216ff3daa151bdc70
MD WASIM AKRAM <wasimkhanktr@gmail.com>
2024-10-24 01:41:12 +0530

    Creating and Initializing an Array

---
+++ Arrays01.java
@@ -0,0 +1,16 @@
+package arrays.java.zn;
+
+public class Arrays01 {
+
+	public static void main(String[] args) {
+		int arr[]= {1,2,3,4};
+		
+		System.out.print("[ ");
+		for(int a : arr)
+		{
+			System.out.print(a+" ");
+		}
+		System.out.print("]");
+	}
+
+}
