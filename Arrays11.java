79affef8d0b40f89facf95f2a96f2239807be38b
MD WASIM AKRAM <wasimkhanktr@gmail.com>
2024-10-24 03:04:46 +0530

    Sorting Arrays

---
+++ Arrays11.java
@@ -0,0 +1,15 @@
+package arrays.java.zn;
+
+import java.util.Arrays;
+
+public class Arrays11 {
+
+	public static void main(String[] args) {
+		int arr[]= {9,2,7,4,11,8};
+		Arrays.sort(arr);
+		
+		
+		System.out.println("New Array is : "+Arrays.toString(arr));
+	}
+
+}
