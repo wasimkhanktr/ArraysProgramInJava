9c10e31f49488382ced789c20f19b4f8d66cd01f
MD WASIM AKRAM <wasimkhanktr@gmail.com>
2024-10-24 02:16:27 +0530

    Multi-dimensional Arrays

--- Arrays06.java
+++ Arrays06.java
@@ -3,7 +3,9 @@ package arrays.java.zn;
 public class Arrays06 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int mularr[][] = {{1,2},{3,4},{5,6}};
+		
+		System.out.println("Elements at row 1 col 1 : "+mularr[0][0]);
 
 	}
 
